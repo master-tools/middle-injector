b6efd23dca63d5dcb5553305750b4f36e15bd872
Alberto  Iglesias Gallego <alberto.uchiha@gmail.com>
2016-11-25 17:17:13 +0100

    ADD(test/*, lib/*): add skeleton
    Add the skeleton to work on it.

---
+++ test/modules/switch/switch.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const assert = require('assert');
+let middleFakerSwitch = require('modules/swich/swich');
+
+describe('Switch module: ', () => {
+    before((done) => {
+        done();
+    });
+
+    after((done) => {
+        done();
+    });
+
+    it('Should select express when the parameter indicate it.', (done) => {
+        middleFakerSwit
+        done();
+    });
+
+
+});
