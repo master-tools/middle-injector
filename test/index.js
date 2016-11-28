f323989cb17de4328722c779bcdf09b9e8b6b96f
Alberto  Iglesias Gallego <alberto.uchiha@gmail.com>
2016-11-28 20:45:49 +0100

    UPDATE(package.json, index.js, switch.js): Change name of the project.
    Change name of the project from "middleFaker" to "middle-inyector". I think it is more correct for this project.

--- test/index.js
+++ test/index.js
@@ -2,9 +2,9 @@
 
 const assert = require('assert');
 const expect = require('chai').expect;
-let middleFaker = require('../index');
+let middleInyector = require('../index');
 
-describe('Middlefaker app: ', () => {
+describe('MiddleInyector app: ', () => {
     before((done) => {
         done();
     });
@@ -14,19 +14,19 @@ describe('Middlefaker app: ', () => {
     });
 
     it('Should return an express object when createFake is called', (done) => {
-        let fakeApp = middleFaker();
+        let fakeApp = middleInyector();
         expect(fakeApp).to.be.a('object');
         done();
     });
 
     it('Should return an express object when createFake is called with all parameters', (done) => {
-        let fakeApp = middleFaker('express', [], []);
+        let fakeApp = middleInyector('express', [], []);
         expect(fakeApp).to.be.a('object');
         done();
     });
 
     it('Should return an init function on express object when createFake is called', (done) => {
-        let fakeApp = middleFaker();
+        let fakeApp = middleInyector();
         expect(fakeApp.init).to.be.a('function');
         done();
     });
--- test/modules/switch/switch.js
+++ test/modules/switch/switch.js
@@ -2,7 +2,7 @@
 
 const assert = require('assert');
 let expressModule = require('modules/express/express');
-let middleFakerSwitch = require('modules/switch/switch');
+let middleInyectorSwitch = require('modules/switch/switch');
 
 describe('Switch module: ', () => {
     before((done) => {
@@ -14,13 +14,13 @@ describe('Switch module: ', () => {
     });
 
     it('Should select express when the parameter indicate it.', (done) => {
-        let returnedModule = middleFakerSwitch.switch('express');
+        let returnedModule = middleInyectorSwitch.switch('express');
         assert.deepEqual(returnedModule, expressModule);
         done();
     });
 
     it('Should select express by default when the paramenter does not indicate it.', (done) => {
-        let returnedModule = middleFakerSwitch.switch('express');
+        let returnedModule = middleInyectorSwitch.switch('express');
         assert.deepEqual(returnedModule, expressModule);
         done();
     });
