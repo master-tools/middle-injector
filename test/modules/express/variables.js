ff5b4b664160de7121f8823c103a658acfa7b0e5
Alberto  Iglesias Gallego <alberto.uchiha@gmail.com>
2016-11-28 19:25:16 +0100

    TEST(variables.js): update test
    Add test for variables express module.

--- test/modules/express/variables.js
+++ test/modules/express/variables.js
@@ -2,6 +2,7 @@
 
 const assert = require('assert');
 const expect = require('chai').expect;
+const mocksVariables = require('../mocks/express/variables');
 let variablesModule = require('modules/express/variables');
 
 describe('Variables express module: ', () => {
@@ -15,6 +16,25 @@ describe('Variables express module: ', () => {
 
     it('Should have load function.', (done) => {
         expect(variablesModule.load).to.be.a('function');
+        done();
+    });
+
+    it('Should return an app Object when load function is called', (done) => {
+        let appObject = variablesModule.load(require('express'), []);
+        expect(appObject).to.be.a('function');
+        done();
+    });
+
+    it('Should set variable "someStuff" to an express app', (done) => {
+        let app = variablesModule.load(require('express')(), mocksVariables.configuration);
+        assert.equal(app.settings.someStuff, mocksVariables.configuration[0].object);
+        done();
+    });
+
+    it('Should set variables "someStuff" and "someStuffTwo" to express app', (done) => {
+        let app = variablesModule.load(require('express')(), mocksVariables.twoVariables);
+        assert.equal(app.settings.someStuff, mocksVariables.twoVariables[0].object);
+        assert.equal(app.settings.someStuffTwo, mocksVariables.twoVariables[1].object);
         done();
     });
 });
