5f528e16615d2f5bafd13a4755fd1ed4e08a6700
Alberto  Iglesias Gallego <alberto.uchiha@gmail.com>
2016-11-28 19:28:32 +0100

    UPDATE(index.js, dependencies.js, express.js, variables.js): update funcionality
    Update the all functionality of the express module and use it on index.

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 'use strict';
 
+let switchModule = require('modules/switch/switch');
 
+let createFake = (framework = 'express', dependencies = [], variables  = []) => {
+    let selectedFramework = switchModule.switch(framework);
+    selectedFramework.init(dependencies, variables);
+    return selectedFramework;
+};
+
+/**
+ * Expose `createApplication()`.
+ */
+
+exports = module.exports = createFake;
