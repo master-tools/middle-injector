aa662c98d1dedb30d7eb506f9480d001ccfd222b
Alberto  Iglesias Gallego <alberto.uchiha@gmail.com>
2016-11-29 19:52:58 +0100

    UPDATE(switch.js): Remove unnecessary code
    Remove unnecessary default on switch and improve their test with a testify switch case.

--- test/modules/switch/switch.js
+++ test/modules/switch/switch.js
@@ -2,6 +2,7 @@
 
 const assert = require('assert');
 let expressModule = require('modules/express/express');
+let restifyModule = require('modules/restify/restify');
 let middleInyectorSwitch = require('modules/switch/switch');
 
 describe('Switch module: ', () => {
@@ -20,8 +21,14 @@ describe('Switch module: ', () => {
     });
 
     it('Should select express by default when the paramenter does not indicate it.', (done) => {
-        let returnedModule = middleInyectorSwitch.switch('express');
+        let returnedModule = middleInyectorSwitch.switch();
         assert.deepEqual(returnedModule, expressModule);
+        done();
+    });
+
+    it('Should select restify by default when the paramenter indicate it.', (done) => {
+        let returnedModule = middleInyectorSwitch.switch('restify');
+        assert.deepEqual(returnedModule, restifyModule);
         done();
     });
 });
