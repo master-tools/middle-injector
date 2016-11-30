b00ee0ff97353ab01419abd62013252122a58c86
Alberto Iglesias Gallego <alberto.uchiha@gmail.com>
2016-12-01 00:32:42 +0100

    UPDATE(*.js): Add js doc
    Add documentation of all functions on repository.

--- index.js
+++ index.js
@@ -2,14 +2,16 @@
 
 let switchModule = require('./lib/modules/switch/switch');
 
+/**
+ * Create app function to select the framework with switch module, inject dependencies and variables on it
+ * @param framework
+ * @param dependencies
+ * @param variables
+ */
 let createApp = (framework = 'express', dependencies = [], variables  = []) => {
     let selectedFramework = switchModule.switch(framework);
     selectedFramework.init(dependencies, variables);
     return selectedFramework;
 };
-
-/**
- * Expose `createApp()`.
- */
 
 exports = module.exports = createApp;
